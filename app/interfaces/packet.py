5b1c5483311cac78b478506a28654f08a0c49bc3
heonjin <hj.cho@crefle.com>
2025-08-26 11:15:42 +0900

    add decorator and rename parameter

--- app/interfaces/packet.py
+++ app/interfaces/packet.py
@@ -2,12 +2,14 @@ from abc import ABC, abstractmethod
 
 
 class PacketInterface(ABC):
+    @classmethod
     @abstractmethod
-    def to_packet(self, packet: bytes) -> bytes:
+    def to_packet(cls, data: bytes) -> bytes:
         """데이터를 패킷(bytes)으로 직렬화"""
-        pass
+        ...
 
+    @classmethod
     @abstractmethod
-    def from_packet(self, packet: bytes) -> bytes:
+    def from_packet(cls, packet: bytes) -> bytes:
         """패킷(bytes)을 데이터로 역직렬화"""
-        pass
+        ...

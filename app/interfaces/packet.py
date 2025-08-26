92b1f9fa804946bdceb1948314ed29d9dd61d0dd
heonjin <hj.cho@crefle.com>
2025-08-26 11:11:32 +0900

    renaing

--- app/interfaces/packet.py
+++ app/interfaces/packet.py
@@ -3,11 +3,11 @@ from abc import ABC, abstractmethod
 
 class PacketInterface(ABC):
     @abstractmethod
-    def to_bytes(self) -> bytes:
+    def to_packet(self, packet: bytes) -> bytes:
         """데이터를 패킷(bytes)으로 직렬화"""
         pass
 
     @abstractmethod
-    def from_bytes(cls, data: bytes):
+    def from_packet(self, packet: bytes) -> bytes:
         """패킷(bytes)을 데이터로 역직렬화"""
         pass

6ab82871d7ba72a05843f9c4d48ce2681db67baf
heonjin <hj.cho@crefle.com>
2025-08-28 15:35:29 +0900

    잘못 커밋

--- app/data.py
+++ app/data.py
@@ -11,33 +11,19 @@ TRecv = TypeVar("TRecv", bound="ReceivedData")
 TSend = TypeVar("TSend", bound="SendData")
 
 
-@dataclasses.dataclass(frozen=True)
-class ReceivedData:
-    cmd: str
-    data: List[str]
-
+class ReceivedData(abc.ABC):
     @classmethod
-    def from_bytes(cls, data: bytes):
-        data = data.decode("utf-8")
-        split_data = data.split("#")
-
-        if len(split_data) == 1:
-            return cls(cmd=split_data[0], data=[])
-
-        return cls(cmd=split_data[0], data=split_data[1:])
+    @abstractmethod
+    def from_bytes(cls, data: bytes) -> Self:
+        """패킷(bytes)을 데이터로 역직렬화"""
+        ...
 
 
-@dataclasses.dataclass(frozen=True)
-class SendData:
-    cmd: str
-    data: List[str] = dataclasses.field(default_factory=list)
-
+class SendData(abc.ABC):
+    @abstractmethod
     def to_bytes(self) -> bytes:
-        result = self.cmd
-        for datum in self.data:
-            result += f"#{datum}"
-
-        return result.encode("utf-8")
+        """데이터를 패킷(bytes)으로 직렬화"""
+        ...
 
 
 @dataclass(slots=True)
